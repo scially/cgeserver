9efd75e0d23405400fb833c94ec2ad88724c59c6
imhwnag <imhwang@126.com>
2024-01-22 21:54:40 +0800

    init

---
+++ app/router/__init__.py
@@ -0,0 +1,3 @@
+from .response import ResponseModel as ResponseModel
+from . import ssr as ssr
+from . import ws as ws
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from router import ssr
-from config import PORT
+from app.router import ssr
+from app import config
 
 app = FastAPI()
 app.include_router(ssr.router)
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, port=PORT)
+    uvicorn.run(app, port=config.PORT)
